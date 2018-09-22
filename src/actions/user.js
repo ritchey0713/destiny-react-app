995ba164ed663dd056b50b69f82898aaec43f01c
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-21 20:36:59 -0400

    add action

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,4 +1,4 @@
-const API_KEY = process.evn.REACT_APP_API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY
 const API_URL = "https://www.bungie.net/Platform/Destiny2"
 
 const getUser = user => {
@@ -12,6 +12,16 @@ const getUser = user => {
 //https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/2/hoombauer
 export const grabUser = (userName, platform) => {
   return dispatch => {
-    return fetch(`${API_URL}/platform/userName`)
+    return fetch(`${API_URL}/platform/userName`, {
+      method: "GET",
+      headers: {
+        'content-Type': 'application/json',
+        "X-API-Key": API_KEY
+      }
+      .then(response => response,json())
+      .then(user => {
+        dispatch(getUser(user))
+      })
+    })
   }
 }
