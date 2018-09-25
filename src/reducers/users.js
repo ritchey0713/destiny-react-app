79a9ebdc067f9970421ec731f9369d778a5ab755
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-25 17:29:50 -0400

    fix merger

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -6,18 +6,14 @@ export default (state = initialState, action) => {
 
   switch (action.type) {
     case "GET_USER_INFO":
-<<<<<<< HEAD
-    debugger
-    return { 
-      ...state,
-      users: action.user.Response[0]}
-=======
+
+
       // debugger
       return {
         ...state,
         user: action.user.Response[0]
       }
->>>>>>> 94f230cfce760d1ca86bb91da9b01f9f1ac19ed4
+
 
     default:
       return state
