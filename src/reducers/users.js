db8362db2d66a134b2be86dabf98880a396deedf
Dj Ritchey <36165081+ritchey0713@users.noreply.github.com>
2018-09-25 17:28:05 -0400

    fix state

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,7 +3,9 @@ export default (state = [], action) => {
   switch (action.type) {
     case "GET_USER_INFO":
     debugger
-    return action.user.Response[0]
+    return { 
+      ...state,
+      users: action.user.Response[0]}
 
     default:
       return state
